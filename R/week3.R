6f2b0e4b7afb157c23eda75422e8be046484aa35
Isaac M Bazian <bazia001@umn.edu>
2023-02-01 17:16:25 -0600

    Cleaned data. No doubt that my solution on line 9 to exclude June 2017 participants is way more complicated than necessary, but it works, and if there were any participants before June 2017 it would keep them as well.

--- R/week3.R
+++ R/week3.R
@@ -5,6 +5,10 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 raw_df <- read.csv(file = "../data/week3.csv", header = T)
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- subset(raw_df, subset = (unclass(raw_df$timeStart) >= unclass(as.POSIXct("2017-07-01 00:00:00"))) | unclass(raw_df$timeStart) < unclass(as.POSIXct("2017-06-01 00:00:00")))
+clean_df <- subset(clean_df, subset = (clean_df$q6 == 1))
+
+#Analysis
 
 
 
