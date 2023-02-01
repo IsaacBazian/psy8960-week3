574e27ead2ca39c3764326e2ec77893f9377b940
Isaac M Bazian <bazia001@umn.edu>
2023-02-01 16:54:28 -0600

    Created df and converted times to POSIXct

--- R/week3.R
+++ R/week3.R
@@ -1,5 +1,10 @@
 #Script Setting and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+#Data Import and Cleaning
+raw_df <- read.csv(file = "../data/week3.csv", header = T)
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+
 
 
